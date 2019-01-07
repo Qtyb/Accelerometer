5225c58ea4ae5db3e8db2c9efe6abd53fb35b2bf
Fenrir999 <spornakarol@gmail.com>
2019-01-07 01:46:16 +0100

    Class with coordinates made by me

---
+++ app/src/main/java/Coordinates.java
@@ -0,0 +1,9 @@
+public class Coordinates {
+    double latitude;
+    double longitude;
+
+    public Coordinates(double latitude, double longitude){
+        this.latitude = latitude;
+        this.longitude  =longitude;
+    }
+}
